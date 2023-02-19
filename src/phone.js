f4ba5e11321e022ea8d950e970b977e1cdb3dcb5
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-02-19 11:41:37 +0200

    Fix style on phone screens

--- src/phone.js
+++ src/phone.js
@@ -1,15 +1,20 @@
 export function showNavBar() {
     const navBar = document.querySelector(".navBar");
     const menuBtn = document.querySelector(".showMore");
-    const content = document.querySelector(".content");
     menuBtn.classList.toggle("active");
     navBar.classList.toggle("show");
     navBar.classList.toggle("shadow-lg");
-    content.style.backgroundColor = navBar.classList.contains("show") ? "#859280" : "#c9d9c2";
 };
 
 export default function addMenuBtn() {
     const menuBtn = document.querySelector(".showMore");
+    const content = document.querySelector(".content");
+
+    content.addEventListener("click", () => {
+        if (menuBtn.classList.contains("active")) {
+            showNavBar();
+        };
+    });
 
     menuBtn.addEventListener("click", () => {
         showNavBar();
